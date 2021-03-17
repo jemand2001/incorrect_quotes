7bc60fc1d37a13f6dd2ded50af41e724c25faa95
jemand2001 <jmand@gmx.de>
2021-03-17 21:55:49 +0100

    comments and stuff

--- quoteGen.js
+++ quoteGen.js
@@ -12,17 +12,13 @@ const numChars = 6;
 // so this disables the name inputs after the first n
 function disableChars(e) {
   const val = Number.parseInt(e.target.value);
-  if (val > numChars) {
-    showMessage("tooHigh");
-    e.target.value = numChars;
-    return;
-  }
   const inputs = [...document.getElementById('charInputs').children];
   for (let i = 0; i < numChars; i++) {
     inputs[i].disabled = i >= val;
   }
 }
 
+// this generates the prompt based on form values
 async function generatePrompt(e) {
   e.stopPropagation()
   e.preventDefault()
@@ -42,6 +38,8 @@ async function generatePrompt(e) {
   renderPrompt(chars, p);
 }
 
+// i thought, since many people just share the text, i'd make it really easy
+// so, when you click on the prompt, it's immediately copied
 function copyContent(e) {
   navigator.permissions.query({name: "clipboard-write"}).then(result => {
     if (result.state == "granted" || result.state == "prompt") {
@@ -51,13 +49,15 @@ function copyContent(e) {
   });
 }
 
+// random choice, meh
 function choice(arr) {
   return arr[Math.floor(Math.random() * arr.length)]
 }
 
+// sets the max of a number input, meh
 function setMax(e) {
-  console.log(e);
-  e.target.max = numChars;
+  if (!e.target.max)
+    e.target.max = numChars;
 }
 
 function showMessage(id) {
@@ -66,7 +66,16 @@ function showMessage(id) {
   setTimeout(() => m.classList.add("invisible"), 1000);
 }
 
+/** 
+ * actually renders the prompt
+ * 
+ * @param chars string[] - the names of the characters
+ * @param p string - the prompt template
+ */
 function renderPrompt(chars, p) {
+  // instead of a bunch of
+  // if (n == "One")
+  // i just go over the names and replace the corresponding template with each name
   for (let i = 0; i < chars.length; i++) {
     const c = String.fromCharCode(65 + i);
     p = p.replaceAll(`{${c}}`, chars[i]);
